42e05d89be1ee902b81e976d10197adf31f27beb
Robson Alves <111312704+robsonalvesdev@users.noreply.github.com>
2023-11-07 16:50:59 -0300

    feat: toast notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
 import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
@@ -32,7 +33,7 @@ import { HomeComponent } from './modules/home/home.component';
     ButtonModule,
     ToastModule,
   ],
-  providers: [CookieService],
+  providers: [CookieService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
 import { UserService } from 'src/app/services/user/user.service';
@@ -25,9 +26,10 @@ export class HomeComponent {
   });
 
   constructor(
-    private formBuilder: FormBuilder, 
+    private formBuilder: FormBuilder,
     private userService: UserService,
-    private cookieService: CookieService
+    private cookieService: CookieService,
+    private messageService: MessageService
   ) {}
 
   onSubmitLoginForm(): void {
@@ -37,8 +39,22 @@ export class HomeComponent {
         next: (response) => {
           this.cookieService.set('USER_INFO', response?.token);
           this.loginForm.reset;
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Sucesso',
+            detail: `Bem vindo de volta ${response.name}!`,
+            life: 2000
+          })
         },
-        error: (err) => console.log(err)
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao fazer o login!',
+            life: 2000
+          });
+          console.log(err);
+        }
       });
     }
   }
@@ -51,8 +67,22 @@ export class HomeComponent {
           console.log('Usuário criado com sucesso', response);
           this.signupForm.reset;
           this.loginCard = true;
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Sucesso',
+            detail: 'Usuário criado com sucesso!',
+            life: 2000
+          })
         },
-        error: (err) => console.log(err)
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao criar novo usuário!',
+            life: 2000
+          });
+          console.log(err)
+        }
       });
     }
   }
